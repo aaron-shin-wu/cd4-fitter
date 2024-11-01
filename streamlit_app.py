752b8570dc2a7fd6a068360c220869d39f9f102b
aaron-shin-wu <106301653+aaron-shin-wu@users.noreply.github.com>
2024-11-01 18:02:17 +0000

    added scipy, adding cd4 functions, added solver_simplex.py.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# User input boxes
+num1 = st.number_input("Box 1", value = 0.0)
+num2 = st.number_input("Box 2", value = 0.0)
+
+# Calculate sum
+result = num1 + num2
+
+# Display result
+st.write("The sum is:", result)
